f75c8edfc36434da56155af9425b8772bd15b14b
gnikwerdna <andrew.king.au@gmail.com>
2015-05-28 11:24:13 +1000

    28052015 update 3

---
+++ StaffSystem/ViewModels/AssignedComplianceProductData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StaffSystem.ViewModels
+{
+    public class AssignedComplianceProductData
+    {
+        public int ComplianceID { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+        public int level { get; set; }
+        public int grp { get; set; }
+        public int order { get; set; }
+    }
+}
